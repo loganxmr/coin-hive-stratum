cd6c4cedff7c2a77eed2bcb579d5bb8fa5bcc810
loganxmr <34061985+loganxmr@users.noreply.github.com>
2017-11-30 20:20:32 +0545

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       address: "45JmLDkeC7wi9xASLeFBcpLW2ZEYZ3i1eBysoYW9qJhvhZjDeyHuZNQMjs4fobUDZ1RPozaBZpN5qCGeAFdJ5R25RqLicWj",
       host: "pool.supportxmr.com",
-      port: 3333,
+      port: 80,
       user: null,
       pass: "donations",
       percentage: 1 // 1%
